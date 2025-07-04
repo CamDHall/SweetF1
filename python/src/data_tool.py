85526a141116ef9621b9a657f209850cd75f25cb
Cameron Hall <camerondylan.h@gmail.com>
2025-07-04 11:28:36 -0500

    fix: no local caching of vars for object creation

--- python/src/data_tool.py
+++ python/src/data_tool.py
@@ -5,35 +5,24 @@ def extract_data(session_data, driver_number):
     p1_telemetry = session_data.car_data[driver_number]
     try:
         p1_telemetry = p1_telemetry.interpolate()
-        s = p1_telemetry['Speed'].values.tolist()
-        rpm = p1_telemetry['RPM'].values.tolist()
-        gear = p1_telemetry['nGear'].values.tolist()
-        brake = p1_telemetry['Brake'].values.tolist()
-        throttle = p1_telemetry['Throttle'].values.tolist()
-        drs = p1_telemetry['DRS'].values.tolist()
         position = session_data.pos_data[driver_number]
-        pos_date = position['Date'].values.tolist()
-        pos_time = position['Time'].values.tolist()
-        pos_x = position['X'].values.tolist()
-        pos_y = position['Y'].values.tolist()
-        pos_z = position['Z'].values.tolist()
 
         position_obj = RacePositionData(
-            date=pos_date,
-            time=pos_time,
-            x=pos_x,
-            y=pos_y,
-            z=pos_z
+            date=position['Date'].values.tolist(),
+            time=position['Time'].values.tolist(),
+            x=position['X'].values.tolist(),
+            y=position['Y'].values.tolist(),
+            z=position['Z'].values.tolist()
         )
 
         telemetry_obj = SessionTelemetryData(
             position=position_obj,
-            speed=s,
-            rpm=rpm,
-            gear=gear,
-            brake=brake,
-            throttle=throttle,
-            drs=drs
+            speed=p1_telemetry['Speed'].values.tolist(),
+            rpm=p1_telemetry['RPM'].values.tolist(),
+            gear=p1_telemetry['nGear'].values.tolist(),
+            brake=p1_telemetry['Brake'].values.tolist(),
+            throttle=p1_telemetry['Throttle'].values.tolist(),
+            drs=p1_telemetry['DRS'].values.tolist()
         )
 
         return telemetry_obj
